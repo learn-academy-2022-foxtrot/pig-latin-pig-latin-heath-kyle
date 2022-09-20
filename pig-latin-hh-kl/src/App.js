d8b714c881f15bab730f4f549c9877d3b1f864a5
let <kyle.tran.le@gmail.com>
2022-09-20 10:37:45 -0700

    pseudo code

---
+++ pig-latin-hh-kl/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/VowelFunctionality"
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Pig Latin Translator!</h1>
+    </>
+  
+  );
+}
+
+export default App;
---
+++ pig-latin-hh-kl/src/components/VowelFunctionality.js
@@ -0,0 +1,47 @@
+//vowel functionality 
+//Input: Apple 
+//*Person clicks submit button*
+//Output: Appleway
+//let vowels = [a, e, i, o, u]
+//if english = "";
+//if (vowels.indexOf(str[0]))
+//return i[1.length] + way
+//function that goes through the first index of each word and if that first letter is a vowel then the app will add yay to the end of the first value of that string
+
+// const numbersArray = [1, 2, 7, 4, 10, 8, 9]
+
+// const onlyEven = (array) => {
+//   return array.filter((value) => value % 2 === 0)
+// }
+
+// console.log(onlyEven(numbersArray))
+// // output: [2, 4, 10, 8]
+
+let vowels = [a, e, i, o, u]
+let translator = (array) => {
+    // going to have to return the original array + new array of "way"
+    return array.map((value)) 
+}
+// console log the original array + adding way
+//console.log(array.push("way"))
+
+
+//we need to remove the first letter .shift
+
+
+
+
+
+
+
+
+
+
+const VowelFunc = () => {
+    return(
+        <>
+    
+        </>
+    )
+}
+export default VowelFunc
